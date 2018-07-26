9d852c1a1f8a2bd29afb39b9c834a5ceb8b8a2ff
Edward Wilson <ed@lighthouseandgiant.co.uk>
2018-07-26 20:12:24 +0100

    footer work

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,65 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './font.css'
+
+const ms = require('simple-modular-scale')
+
+const scale = ms({
+  base: 16,
+  ratios: [3 / 2, 4 / 3],
+  length: 8,
+})
+// [ 16, 24, 32, 48, 64, 96, 128, 192 ]
+
+const getYear = new Date().getFullYear()
 
 const Footer = ({ siteTitle }) => (
   <footer
     style={{
-      paddingTop: '1.45rem',
-      paddingBottom: '1.45rem',
+      paddingTop: scale[1],
+      paddingBottom: scale[3],
       display: 'flex',
       justifySelf: 'flex-end',
+      flexDirection: 'row',
+      position: 'fixed',
+      bottom: '0',
+      left: '0',
+      right: '0',
+      paddingLeft: scale[4],
+      paddingRight: scale[4],
+      justifyContent: 'space-between',
     }}
   >
-    <Link
-      to="/"
-      style={{
-        color: 'white',
-        textDecoration: 'none',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: '#ffffff',
-        display: 'block',
-      }}
-    />
+    <div className="" style={{ display: 'flex', alignSelf: 'flex-end' }}>
+      <Link
+        to="/about"
+        style={{
+          color: '#111111',
+          textDecoration: 'none',
+          display: 'flex',
+          fontFamily: 'bagnard_regularregular',
+          fontSize: scale[0],
+          textTransform: 'uppercase',
+          letterSpacing: '1px',
+          alignSelf: 'flex-start',
+        }}
+      >
+        Wtf
+      </Link>
+    </div>
+    <div className="" style={{ display: 'flex', alignSelf: 'flex-end' }}>
+      <p
+        style={{
+          fontFamily: 'bagnard_regularregular',
+          fontSize: scale[0],
+          textTransform: 'uppercase',
+          letterSpacing: '1px',
+          marginBottom: '0',
+        }}
+      >
+        &copy; 2006 - {getYear}
+      </p>
+    </div>
   </footer>
 )
 
