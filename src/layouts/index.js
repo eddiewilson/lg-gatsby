ae579da24057fdd5db5ea16e1a2334a3ec469e00
Edward Wilson <ed@lighthouseandgiant.co.uk>
2019-06-10 19:53:29 +0100

    added desc andn meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,8 +18,16 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: 'description',
+          content:
+            'Lighthouse & Giant is a design focused duo in Falmouth Cornwall working with small businesses and startups. We have designed and named numerous brands, built modern web applications, used our design thinking and illustration skills to help small acorns become mighty oaks.',
+        },
+        {
+          name: 'keywords',
+          content:
+            'Design, Digital Design, Branding, Naming, Cornwall Design, Cornwall Illustration, Animation, Logo Design, Brand Strategy',
+        },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
